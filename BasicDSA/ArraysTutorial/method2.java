cdd6bb798ba0e50cf304b98b2e23621361183c40
Kishan Jaiswal <145869984+kishanjaiswal1515@users.noreply.github.com>
2025-09-18 05:26:37 +0000

    changesDone

---
+++ BasicDSA/ArraysTutorial/method2.java
@@ -0,0 +1,10 @@
+package ArraysTutorial;
+
+public class method2 {
+    static void seeMessage(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args) {
+        method2.seeMessage();
+    }
+}
---
+++ BasicDSA/ArraysTutorial/methodsCall.java
@@ -0,0 +1,13 @@
+package ArraysTutorial;
+
+public class methodsCall {
+
+    public void messageRead(){
+        System.out.println("hello, kishan");
+    }
+    public static void main(String[] args) {
+        methodsCall obj = new methodsCall();
+
+        obj.messageRead();
+    }
+}
